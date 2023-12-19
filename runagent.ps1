e619aeab54823b54f7817dabe66af86180094334
Salil Ponde <salil.ponde@gmail.com>
2023-12-19 16:56:32 +0530

    Cleanup

--- runagent.ps1
+++ runagent.ps1
@@ -8,6 +8,5 @@ go build .\cmd\agent\
 
 # Run
 $env:SERVER_URL="http://localhost:5173"
-$env:TOKEN="uXQ/JvpmhtjiIm7MUdcUKC47/bIv7LyYDd3CLDvR0ixkUOYy4bIwog=="
 $env:LOG_LEVEL="DEBUG"
 .\agent.exe
--- runagent.sh
+++ runagent.sh
@@ -6,6 +6,5 @@ go build ./cmd/agent
 
 # Run 
 export SERVER_URL="http://192.168.1.7:9090"
-export TOKEN="1ZRXtM8ZCMVjP2jIXV99GYLeWfXgN4j0okzK0NaRpjosuIYU5wVoCEA=="
 export LOG_LEVEL="DEBUG"
 ./agent
